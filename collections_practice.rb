f974a60b5516c9d77c0faa1263147fcc176b2aea
Melissa Williams <melissa@brandmeetscopy.com>
2019-05-02 23:23:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -60,13 +60,14 @@ def remove_non_strings(array)
 
   def find_cool(hash)
     cool_hash = []
-    hash.each do |key, value|
+    hash.each do |new_hash|
+      new_hash.each do |key, value
       if key == [:temperature] && value == ["cool"]
         cool_hash << [:temperature]["cool"]
       end
+      end
       cool_hash
     end
-
   end
 
   def organize_schools(school_hash)
